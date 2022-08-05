fd1e0906db4bf69565712dd3a4ee89c08f4269ff
Emily Xiong <xiongemi@gmail.com>
2022-08-05 17:09:46 -0400

    feat(react-native): upgrade metro to 0.72.0

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,15 +1,15 @@
 export const nxVersion = require('../../package.json').version;
 
 export const reactNativeVersion = '0.69.3';
-export const typesReactNativeVersion = '0.69.3';
+export const typesReactNativeVersion = '0.69.5';
 
 export const typesNodeVersion = '16.11.7';
 
-export const metroVersion = '0.71.3';
+export const metroVersion = '0.72.0';
 
-export const reactNativeCommunityCli = '8.0.4';
+export const reactNativeCommunityCli = '8.0.5';
 export const reactNativeCommunityCliIos = '8.0.4';
-export const reactNativeCommunityCliAndroid = '8.0.4';
+export const reactNativeCommunityCliAndroid = '8.0.5';
 
 export const reactNativeConfigVersion = '1.4.6';
 export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.7';
